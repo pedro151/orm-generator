4d21140bcb68da1c376f1ea28b85a37bca31e3fe
pedro151 <phacl@hotmail.com>
2016-02-26 15:42:27 -0300

    change testes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,13 @@
 
 function __autoload ( $class )
 {
-    $parts = explode ( '\\', $class );
-    require dirname ( __FILE__ ) .DIRECTORY_SEPARATOR. implode ( DIRECTORY_SEPARATOR, $parts ) . '.php';
+    $parts = explode ( '\\' , $class );
+    $file = dirname ( __FILE__ ) . DIRECTORY_SEPARATOR
+            . implode ( DIRECTORY_SEPARATOR , $parts ) . '.php';
+    if ( ! is_file ( $file ) )
+    {
+        $file = dirname ( __FILE__ ) . '../' . DIRECTORY_SEPARATOR
+                . implode ( DIRECTORY_SEPARATOR , $parts ) . '.php';
+    }
+    require $file;
 }
