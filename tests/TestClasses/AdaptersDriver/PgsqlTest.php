c947330b0aa50d08132eaae8beb2e67efdbd036e
pedro151 <phacl@hotmail.com>
2016-03-14 11:14:25 -0300

    refatoring  tests

--- tests/TestClasses/AdaptersDriver/PgsqlTest.php
+++ tests/TestClasses/AdaptersDriver/PgsqlTest.php
@@ -8,15 +8,16 @@
 
 namespace TestClasses\AdaptersDriver;
 
-
+use Classes\AdapterConfig\None;
 use Classes\AdaptersDriver\Pgsql;
 
+/**
+ * Class PgsqlTest
+ *
+ * @package TestClasses\AdaptersDriver
+ */
 class PgsqlTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @type \Classes\AdapterConfig\ZendFrameworkOne
-     */
-    private $objAdapterConfig;
     /**
      * @type Pgsql
      */
@@ -27,50 +28,45 @@ class PgsqlTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp ()
     {
-        $this->pdo = new \PDO( $GLOBALS[ 'db_dsn' ], $GLOBALS[ 'db_username' ], $GLOBALS[ 'db_password' ] );
-        $this->pdo->setAttribute ( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
+        $this->pdo = new \PDO( $GLOBALS[ 'db_dsn' ] , $GLOBALS[ 'db_username' ] , $GLOBALS[ 'db_password' ] );
+        $this->pdo->setAttribute ( \PDO::ATTR_ERRMODE , \PDO::ERRMODE_EXCEPTION );
         $this->pdo->query (
             "CREATE TABLE accounts (
-      account_name      VARCHAR(100) NOT NULL PRIMARY KEY
-    );"
+              account_name      VARCHAR(100) NOT NULL PRIMARY KEY
+            );"
         );
 
         $this->pdo->query (
             "CREATE TABLE products (
-      product_id        INTEGER NOT NULL PRIMARY KEY,
-      product_name      VARCHAR(100)
-    );"
+                  product_id        INTEGER NOT NULL PRIMARY KEY,
+                  product_name      VARCHAR(100)
+                );"
         );
 
         $this->pdo->query (
             "CREATE TABLE bugs (
-      bug_id            INTEGER NOT NULL PRIMARY KEY,
-      bug_description   VARCHAR(100),
-      bug_status        VARCHAR(20),
-      reported_by       VARCHAR(100) REFERENCES accounts(account_name),
-      assigned_to       VARCHAR(100) REFERENCES accounts(account_name),
-      verified_by       VARCHAR(100) REFERENCES accounts(account_name)
-    );"
+                  bug_id            INTEGER NOT NULL PRIMARY KEY,
+                  bug_description   VARCHAR(100),
+                  bug_status        VARCHAR(20),
+                  reported_by       VARCHAR(100) REFERENCES accounts(account_name),
+                  assigned_to       VARCHAR(100) REFERENCES accounts(account_name),
+                  verified_by       VARCHAR(100) REFERENCES accounts(account_name)
+                );"
         );
 
         $this->pdo->query (
             "CREATE TABLE bugs_products (
-      bug_id            INTEGER NOT NULL REFERENCES bugs,
-      product_id        INTEGER NOT NULL REFERENCES products,
-      PRIMARY KEY       (bug_id, product_id)
-    );"
+              bug_id            INTEGER NOT NULL REFERENCES bugs,
+              product_id        INTEGER NOT NULL REFERENCES products,
+              PRIMARY KEY       (bug_id, product_id)
+            );"
         );
 
-        $this->objAdapterConfig = $this->getMockBuilder ( '\Classes\AdapterConfig\ZendFrameworkOne' )
-            ->disableOriginalConstructor ( 0 )
-            ->setMethods ( array ( 'getParams' ) )
-            ->getMock ();
-
-        $this->parseObj ();
-        $this->objDriver = new Pgsql( $this->objAdapterConfig );
-        $this->objDriver->runDatabase ();
     }
 
+    /**
+     *
+     */
     protected function tearDown ()
     {
         $this->pdo->query ( "DROP TABLE bugs_products" );
@@ -79,54 +75,85 @@ class PgsqlTest extends \PHPUnit_Framework_TestCase
         $this->pdo->query ( "DROP TABLE accounts" );
     }
 
-    protected function parseObj ()
+    /**
+     * @return \Classes\AdaptersDriver\Pgsql
+     */
+    protected function getDataBaseDrive ()
     {
-        $class = new \ReflectionClass( '\Classes\AdapterConfig\ZendFrameworkOne' );
-        $property = $class->getProperty ( 'arrConfig' );
-        $property->setAccessible ( true );
-        $property->setValue (
-            $this->objAdapterConfig, array (
-                                       'driver'    => 'pdo_pgsql',
-                                       'host'      => 'localhost',
-                                       'database'  => 'dao_generator',
-                                       'username'  => 'postgres',
-                                       'socket'    => null,
-                                       'password'  => '123',
-                                       'namespace' => ''
-                                   )
+        if ( ! $this->objDriver )
+        {
+            $this->objDriver = new Pgsql( $this->getAdapterConfig () );
+            $this->objDriver->runDatabase ();
+        }
+
+        return $this->objDriver;
+    }
+
+    /**
+     * @return \Classes\AdapterConfig\None
+     */
+    protected function getAdapterConfig ()
+    {
+        $arrConfig = array (
+            'driver'    => 'pdo_pgsql' ,
+            'host'      => 'localhost' ,
+            'database'  => 'dao_generator' ,
+            'username'  => 'postgres' ,
+            'socket'    => null ,
+            'password'  => '123' ,
+            'namespace' => ''
         );
 
-        return $property;
+        return new None( $arrConfig );
     }
 
+    /**
+     *
+     */
     public function testPDO ()
     {
-        $this->assertTrue ( $this->objDriver->getPDO () instanceof \PDO );
+        $this->assertTrue ( $this->getDataBaseDrive ()->getPDO () instanceof \PDO );
     }
 
+    /**
+     *
+     */
     public function testGetListNameTable ()
     {
-        $this->assertTrue ( is_array ( $this->objDriver->getListNameTable () ) );
-        $this->assertTrue ( count ( $this->objDriver->getListNameTable () ) > 0 );
+        $this->assertTrue ( is_array ( $this->getDataBaseDrive ()
+                                            ->getListNameTable () ) );
+        $this->assertTrue ( count ( $this->getDataBaseDrive ()->getListNameTable () )
+                            > 0 );
     }
 
+    /**
+     *
+     */
     public function testGetListColumns ()
     {
-        $this->assertTrue ( is_array ( $this->objDriver->getListColumns () ) );
+        $this->assertTrue ( is_array ( $this->getDataBaseDrive ()->getListColumns () ) );
     }
 
+    /**
+     *
+     */
     public function testGetTables ()
     {
         $this->assertTrue (
-            $this->objDriver->getTable ( "public.accounts", "public" ) instanceof
+            $this->getDataBaseDrive ()->getTable ( "public.accounts" , "public" )
+            instanceof
             \Classes\Db\DbTable
         );
-        $arrTables = $this->objDriver->getTables ( 'public' );
-        $this->assertTrue ( $arrTables[ "public.accounts" ] instanceof \Classes\Db\DbTable );
+        $arrTables = $this->getDataBaseDrive ()->getTables ( 'public' );
+        $this->assertTrue ( $arrTables[ "public.accounts" ] instanceof
+                            \Classes\Db\DbTable );
     }
 
+    /**
+     *
+     */
     public function testTotalTables ()
     {
-        $this->assertTrue ( is_int ( $this->objDriver->getTotalTables () ) );
+        $this->assertTrue ( is_int ( $this->getDataBaseDrive ()->getTotalTables () ) );
     }
 }
