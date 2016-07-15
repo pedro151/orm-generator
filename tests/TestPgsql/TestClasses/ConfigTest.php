ce3e02f8f49a8da5d88e0d70b153663ce3e2de8f
pedro151 <phacl@hotmail.com>
2016-07-15 11:00:05 -0300

    fixed words private

--- tests/TestPgsql/TestClasses/ConfigTest.php
+++ tests/TestPgsql/TestClasses/ConfigTest.php
@@ -28,46 +28,50 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
      *
      * @return mixed
      */
-    private function getReflectionMethodParseConfigEnv ( $configTemp , $argv )
+    private function getReflectionMethodParseConfigEnv ( $configTemp, $argv )
     {
         $obj = $this->getMockBuilder ( 'Classes\Config' )
                     ->disableOriginalConstructor ()
                     ->getMock ();
 
 
-        $reflectionMethod = new \ReflectionMethod( 'Classes\Config' , 'parseConfigEnv' );
+        $reflectionMethod = new \ReflectionMethod( 'Classes\Config', 'parseConfigEnv' );
         $reflectionMethod->setAccessible ( true );
 
-        return $reflectionMethod->invokeArgs ( $obj , array ( $configTemp , $argv ) );
+        return $reflectionMethod->invokeArgs (
+            $obj,
+            array (
+                $configTemp,
+                $argv
+            )
+        );
     }
 
     public function testAdapterDriver ()
     {
         $config = new Config(
             array (
-                'framework' => 'none' ,
-                'database'  => $GLOBALS[ 'dbname' ] ,
+                'framework' => 'none',
+                'database'  => $GLOBALS[ 'dbname' ],
                 'driver'    => 'pgsql'
-            ) ,
-            $this->basePath
+            ), $this->basePath
         );
 
-        $driver = $config->getAdapterDriver ();
-        $table = $driver->getTables ();
+        $driver = $config->getAdapterDriver ( $config->getAdapterConfig() );
+        $table  = $driver->getTables ();
         $this->assertTrue ( $driver instanceof Pgsql );
         $this->assertTrue ( is_array ( $table ) );
     }
 
     public function testAdapterConfig ()
     {
-        $config = new Config(
+        $config    = new Config(
             array (
-                'database' => $GLOBALS[ 'dbname' ] ,
+                'database' => $GLOBALS[ 'dbname' ],
                 'driver'   => 'pgsql'
-            ) ,
-            $this->basePath
+            ), $this->basePath
         );
-        $config = $config->getAdapterConfig ();
+        $config    = $config->getAdapterConfig ();
         $strAuthor = $config->author;
         $this->assertTrue ( $strAuthor == ucfirst ( get_current_user () ) );
         $this->assertTrue ( $config->lol == null );
@@ -77,46 +81,46 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     {
         $param1 = array ( 'main' => 'configs' );
         $param2 = array ();
-        $resp = $this->getReflectionMethodParseConfigEnv ( $param1 , $param2 );
-        $this->assertEquals ( 'configs' , $resp );
+        $resp   = $this->getReflectionMethodParseConfigEnv ( $param1, $param2 );
+        $this->assertEquals ( 'configs', $resp );
     }
 
     public function testParseConfigEnvArgs ()
     {
         $param1 = array (
             'main'    => array (
-                "framework"  => "zf1" ,
-                "database"   => "main" ,
+                "framework"  => "zf1",
+                "database"   => "main",
                 "config-env" => "config2"
-            ) ,
+            ),
             'config1' => array (
-                "extends"  => "main" ,
+                "extends"  => "main",
                 "database" => "config1"
-            ) ,
+            ),
             'config2' => array (
-                "extends"  => "main" ,
+                "extends"  => "main",
                 "database" => "config2"
-            ) ,
+            ),
         );
         $param2 = array ( 'config-env' => 'config1' );
 
         $expectedConfig1 = array (
-            "extends"    => "main" ,
-            "framework"  => "zf1" ,
-            "config-env" => "config2" ,
+            "extends"    => "main",
+            "framework"  => "zf1",
+            "config-env" => "config2",
             "database"   => "config1"
         );
-        $resp1 = $this->getReflectionMethodParseConfigEnv ( $param1 , $param2 );
-        $this->assertEquals ( $expectedConfig1 , $resp1 );
+        $resp1           = $this->getReflectionMethodParseConfigEnv ( $param1, $param2 );
+        $this->assertEquals ( $expectedConfig1, $resp1 );
 
         $expectedConfig2 = array (
-            "extends"    => "main" ,
-            "framework"  => "zf1" ,
-            "config-env" => "config2" ,
+            "extends"    => "main",
+            "framework"  => "zf1",
+            "config-env" => "config2",
             "database"   => "config2"
         );
-        $resp2 = $this->getReflectionMethodParseConfigEnv ( $param1 , array () );
-        $this->assertEquals ( $expectedConfig2 , $resp2 );
+        $resp2           = $this->getReflectionMethodParseConfigEnv ( $param1, array () );
+        $this->assertEquals ( $expectedConfig2, $resp2 );
 
     }
 }
