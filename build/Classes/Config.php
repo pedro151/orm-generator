ec2825ac8828506eb6c017833f055efeb7e07bcb
pedro151 <phacl@hotmail.com>
2016-10-21 20:17:46 -0200

    clean trash

--- build/Classes/Config.php
+++ build/Classes/Config.php
@@ -65,40 +65,45 @@ class Config
     private $adapterDriver;
 
     private $frameworkList = array (
-        'none',
-        'zf1',
+        'none' ,
+        'zf1' ,
         'phalcon'
     );
 
     private $parameterList = array (
-        'init'       => 'Creates the necessary configuration file to start using the orm-generator.',
-        'config-ini' => 'reference to another .ini file configuration (relative path).',
-        'config-env' => 'orm-generator configuration environment.',
-        'framework'  => 'name framework used, which has the contents of the database configurations and framework template.',
-        'driver'     => 'database driver name (Ex.: pgsql).',
-        'database'   => 'database name.',
-        'schema'     => 'database schema name (one or more than one).',
-        'tables'     => 'table name (parameter can be used more then once).',
-        'clean-trash'=> 'delete all files that do not belong to your Database due',
-        'status'     => 'show status of implementation carried out after completing the process.',
-        'version'    => 'shows the version of orm-generator.',
-        'help'       => "help command explaining all the options and manner of use.",
-        'path'       => "specify where to create the files (default is current directory).",
+        'init'        => 'Creates the necessary configuration file to start using the orm-generator.' ,
+        'config-ini'  => 'reference to another .ini file configuration (relative path).' ,
+        'config-env'  => 'orm-generator configuration environment.' ,
+        'framework'   => 'name framework used, which has the contents of the database configurations and framework template.' ,
+        'driver'      => 'database driver name (Ex.: pgsql).' ,
+        'database'    => 'database name.' ,
+        'schema'      => 'database schema name (one or more than one).' ,
+        'tables'      => 'table name (parameter can be used more then once).' ,
+        'clean-trash' => 'delete all files that do not belong to your Database due' ,
+        'status'      => 'show status of implementation carried out after completing the process.' ,
+        'version'     => 'shows the version of orm-generator.' ,
+        'help'        => "help command explaining all the options and manner of use." ,
+        'path'        => "specify where to create the files (default is current directory)." ,
     );
 
-    public function __construct ( $argv, $basePath, $numArgs )
+    public function __construct ( $argv , $basePath , $numArgs )
     {
-        if ( array_key_exists ( 'help', $argv ) or ( $numArgs > 1 && count ( $argv ) < 1 ) ) {
+        if ( array_key_exists ( 'help' , $argv ) or ( $numArgs > 1
+                                                      && count ( $argv ) < 1 )
+        )
+        {
             die ( $this->getUsage () );
         }
-        if ( array_key_exists ( 'version', $argv ) ) {
+        if ( array_key_exists ( 'version' , $argv ) )
+        {
             die ( $this->getVersion () );
         }
-        if ( array_key_exists ( 'status', $argv ) ) {
+        if ( array_key_exists ( 'status' , $argv ) )
+        {
             $argv[ 'status' ] = true;
         }
 
-        $this->argv = $this->parseConfig ( $basePath, $argv );
+        $this->argv = $this->parseConfig ( $basePath , $argv );
     }
 
     /**
@@ -109,7 +114,7 @@ class Config
     public function getUsage ()
     {
         $version = $this->getVersion ();
-        $list    = $this->renderParam ();
+        $list = $this->renderParam ();
 
         return <<<EOF
 parameters:
@@ -123,11 +128,13 @@ EOF;
     public function renderParam ()
     {
         $return = "";
-        foreach ( $this->parameterList as $param => $desc ) {
-            if ( strlen ( $param ) < 5 ) {
+        foreach ( $this->parameterList as $param => $desc )
+        {
+            if ( strlen ( $param ) < 5 )
+            {
                 $return .= "\t--" . $param . "\t\t: " . $desc . "\n";
-            }
-            else {
+            } else
+            {
                 $return .= "\t--" . $param . "\t: " . $desc . "\n";
             }
 
@@ -136,11 +143,32 @@ EOF;
         return $return;
     }
 
+    public function checkHasNewVersion ()
+    {
+        $opts = array (
+            'http' => array (
+                'method' => 'GET' ,
+                'header' => array (
+                    'User-Agent: PHP'
+                )
+            )
+        );
+        $context = stream_context_create($opts);
+        $tags = json_decode ( file_get_contents ( "https://api.github.com/repos/pedro151/orm-generator/tags" , false, $context) );
+
+
+        $lastVersion = preg_replace("/[^0-9.]/", "", $tags[ 0 ]->name);
+        if ( $lastVersion > static::$version )
+        {
+            return "\033[0;31mThere is a new version $lastVersion available:\033[0m https://github.com/pedro151/orm-generator\n";
+        }
+    }
+
     public function getVersion ()
     {
         $version = static::$version;
 
-        return "ORM Generator By: Pedro Alarcao Version: $version\n";
+        return "ORM Generator By: Pedro Alarcao Version: $version\n".$this->checkHasNewVersion();
     }
 
     /**
@@ -152,24 +180,28 @@ EOF;
      * @return array
      * @throws \Exception
      */
-    private function parseConfig ( $basePath, $argv )
+    private function parseConfig ( $basePath , $argv )
     {
-        $this->_basePath =  $basePath;
+        $this->_basePath = $basePath;
 
         $configIni = isset( $argv[ 'config-ini' ] )
-            ? $argv[ 'config-ini' ] : $this->_basePath
-                                      . $this->configIniDefault;
+            ? $argv[ 'config-ini' ]
+            : $this->_basePath
+              . $this->configIniDefault;
 
-        $configTemp    = $this->loadIniFile ( realpath ( $configIni ) );
-        $configCurrent = self::parseConfigEnv ( $configTemp, $argv );
+        $configTemp = $this->loadIniFile ( realpath ( $configIni ) );
+        $configCurrent = self::parseConfigEnv ( $configTemp , $argv );
 
-        if ( !isset( $configCurrent[ 'framework' ] ) ) {
+        if ( ! isset( $configCurrent[ 'framework' ] ) )
+        {
             throw new \Exception( "configure which framework you want to use! \n" );
         }
 
-        if ( !in_array ( $configCurrent[ 'framework' ], $this->frameworkList ) ) {
-            $frameworks = implode ( "\n\t", $this->frameworkList );
-            throw new \Exception( "list of frameworks: \n\t\033[1;33m" . $frameworks . "\n\033[0m" );
+        if ( ! in_array ( $configCurrent[ 'framework' ] , $this->frameworkList ) )
+        {
+            $frameworks = implode ( "\n\t" , $this->frameworkList );
+            throw new \Exception( "list of frameworks: \n\t\033[1;33m" . $frameworks
+                                  . "\n\033[0m" );
         }
 
         return $argv + array_filter ( $configCurrent );
@@ -182,17 +214,21 @@ EOF;
      *
      * @return string
      */
-    private static function parseConfigEnv ( $configTemp, $argv )
+    private static function parseConfigEnv ( $configTemp , $argv )
     {
-        $thisSection = isset( $configTemp[ key ( $configTemp ) ][ 'config-env' ] ) ? $configTemp[ key (
-            $configTemp
-        ) ][ 'config-env' ] : null;
+        $thisSection = isset( $configTemp[ key ( $configTemp ) ][ 'config-env' ] )
+            ? $configTemp[ key (
+                $configTemp
+            ) ][ 'config-env' ] : null;
 
-        $thisSection = isset( $argv[ 'config-env' ] ) ? $argv[ 'config-env' ] : $thisSection;
+        $thisSection = isset( $argv[ 'config-env' ] ) ? $argv[ 'config-env' ]
+            : $thisSection;
 
-        if ( isset( $configTemp[ $thisSection ][ 'extends' ] ) ) {
+        if ( isset( $configTemp[ $thisSection ][ 'extends' ] ) )
+        {
             #faz marge da config principal com a config extendida
-            return $configTemp[ $thisSection ] + $configTemp[ $configTemp[ $thisSection ][ 'extends' ] ];
+            return $configTemp[ $thisSection ]
+                   + $configTemp[ $configTemp[ $thisSection ][ 'extends' ] ];
         }
 
         return $configTemp[ key ( $configTemp ) ];
@@ -211,23 +247,26 @@ EOF;
      */
     protected function loadIniFile ( $filename )
     {
-        if ( !is_file ( $filename ) ) {
+        if ( ! is_file ( $filename ) )
+        {
             throw new \Exception( "\033[0;31mError: configuration file does not exist! \033[0m\n" );
         }
 
-        $loaded   = parse_ini_file ( $filename, true );
+        $loaded = parse_ini_file ( $filename , true );
         $iniArray = array ();
-        foreach ( $loaded as $key => $data ) {
-            $pieces      = explode ( $this->sectionSeparator, $key );
+        foreach ( $loaded as $key => $data )
+        {
+            $pieces = explode ( $this->sectionSeparator , $key );
             $thisSection = trim ( $pieces[ 0 ] );
-            switch ( count ( $pieces ) ) {
+            switch ( count ( $pieces ) )
+            {
                 case 1:
                     $iniArray[ $thisSection ] = $data;
                     break;
 
                 case 2:
-                    $extendedSection          = trim ( $pieces[ 1 ] );
-                    $iniArray[ $thisSection ] = array_merge ( array ( 'extends' => $extendedSection ), $data );
+                    $extendedSection = trim ( $pieces[ 1 ] );
+                    $iniArray[ $thisSection ] = array_merge ( array ( 'extends' => $extendedSection ) , $data );
                     break;
 
                 default:
@@ -246,7 +285,8 @@ EOF;
      */
     private function factoryConfig ()
     {
-        switch ( strtolower ( $this->argv[ 'framework' ] ) ) {
+        switch ( strtolower ( $this->argv[ 'framework' ] ) )
+        {
             case 'zf1':
                 return new ZendFrameworkOne( $this->argv );
             case 'phalcon':
@@ -266,7 +306,8 @@ EOF;
      */
     private function factoryDriver ( AdapterConfig\AbstractAdapter $config )
     {
-        switch ( $this->argv[ 'driver' ] ) {
+        switch ( $this->argv[ 'driver' ] )
+        {
             case 'pgsql':
             case 'pdo_pgsql':
                 return new Pgsql( $config );
@@ -287,7 +328,8 @@ EOF;
      */
     public function getAdapterConfig ()
     {
-        if ( !$this->adapterConfig instanceof AbstractAdapter ) {
+        if ( ! $this->adapterConfig instanceof AbstractAdapter )
+        {
             $this->adapterConfig = $this->factoryConfig ();
         }
 
@@ -299,7 +341,8 @@ EOF;
      */
     public function getAdapterDriver ( AdapterConfig\AbstractAdapter $config )
     {
-        if ( !$this->adapterDriver ) {
+        if ( ! $this->adapterDriver )
+        {
             $this->adapterDriver = $this->factoryDriver ( $config );
         }
 
